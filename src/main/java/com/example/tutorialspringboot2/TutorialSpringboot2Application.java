895ff6d3098ad81cbd7646a0b8e956f47b85299d
dankim <dankim@riotgames.com>
2022-02-20 19:26:46 +0900

    Initial commit

---
+++ src/main/java/com/example/tutorialspringboot2/TutorialSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.example.tutorialspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialSpringboot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tutorialspringboot2/TutorialSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialspringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
